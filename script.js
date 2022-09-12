058d5916eb70665e624111a3f0f28dab90e94bab
andré <113362031+flopps213@users.noreply.github.com>
2022-09-12 12:42:47 -0300

    primeiro envio (e comentario) :D

---
+++ script.js
@@ -0,0 +1,10 @@
+function mudarCor(){
+  document.getElementById("body").style.backgroundColor="white";
+};
+
+function mudarImagemCima() {
+  document.getElementById("imagem1").src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1127px-Big_Floppa_and_Justin_2_%28cropped%29.jpg";
+}
+function mudarImagemFora(){
+ document.getElementById("imagem1").src="http://pm1.narvii.com/8288/e166d2a0d0d8cf2dc73118ddaf2d3b07345e2cecr1-531-578v2_uhq.jpg"
+}
